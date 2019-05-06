97f8e345ced25b33a5c6f23819e662b9e149dc60
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2019-05-06 17:28:17 +0200

    Removed test function in main

--- VirtualPiano/main.cpp
+++ VirtualPiano/main.cpp
@@ -5,35 +5,6 @@
 
 #include "VirtualPianoParser.h"
 
-#include "MxmlFormatter.h"
-void test() {
-	std::vector<std::unique_ptr<Pause>> vec_pauses;
-	vec_pauses.push_back(std::make_unique<Pause>(Pause(MusicSymbol::one_quarter)));
-	vec_pauses.push_back(std::make_unique<Pause>(Pause(MusicSymbol::one_eight)));
-	vec_pauses.push_back(std::make_unique<Pause>(Pause(MusicSymbol::one_quarter)));
-	vec_pauses.push_back(std::make_unique<Pause>(Pause(MusicSymbol::one_eight)));
-
-	std::vector<std::unique_ptr<Note>> vec_notes;
-	vec_notes.push_back(std::make_unique<Note>(Note(Note::C, Note::THREE, MusicSymbol::one_quarter)));
-	vec_notes.push_back(std::make_unique<Note>(Note(Note::C, Note::THREE, MusicSymbol::one_eight)));
-	vec_notes.push_back(std::make_unique<Note>(Note(Note::A, Note::TWO, MusicSymbol::one_quarter)));
-	vec_notes.push_back(std::make_unique<Note>(Note(Note::D, Note::SHARP, Note::FOUR, MusicSymbol::one_eight)));
-
-	Composition<> composition(Duration(3, 4));
-	for (auto i = 0U; i < vec_notes.size(); i++) {
-		std::cout << vec_notes[i]->to_mxml();
-		std::cout << vec_pauses[i]->to_mxml();
-		composition.push_back(std::move(vec_notes[i]), 0);
-		composition.push_back(std::move(vec_pauses[i]), 0);
-	}
-
-	MxmlFormatter<> mxml_formatter;
-	mxml_formatter.generate_output_file(std::cout, composition);
-	
-	system("pause");
-}
-
-
 int main(int argc, char* argv[]) {
 	std::cout << "Hello, World!" << std::endl;
 
@@ -58,8 +29,6 @@ int main(int argc, char* argv[]) {
 		}
 
 	}
-
-	test();
 
 	return 0;
 }
