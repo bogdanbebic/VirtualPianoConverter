ca177abd68ffa9f07b22e520db6f9e9fd4cf3ddd
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2019-05-13 22:21:05 +0200

    Implemented MusicSymbolBmpStruct

---
+++ VirtualPiano/MusicSymbolBmpStruct.h
@@ -0,0 +1,12 @@
+﻿#ifndef _MUSICSYMBOLBMPSTRUCT_H_
+#define _MUSICSYMBOLBMPSTRUCT_H_
+
+namespace bmp_formatter {
+	typedef struct MusicSymbolBmpStruct {
+		unsigned char red, green, blue;
+		unsigned num_of_pixels;
+	} music_symbol_bmp;
+
+}
+
+#endif
